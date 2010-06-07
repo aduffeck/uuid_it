c283440c84c0af8b0f1fd9e3007045d1a5185996
Andre Duffeck <aduffeck@suse.de>
2010-06-07 09:25:04 +0200

    add some tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,22 @@
 require 'rubygems'
 require 'active_support'
 require 'active_support/test_case'
+require 'test/unit'
+require 'active_record'
+ActiveRecord::Base.establish_connection(:adapter => "sqlite3", :database => ":memory:")
+
+require File.dirname(__FILE__) + '/../lib/uuid_it'
+
+silence_stream(STDOUT) do
+  ActiveRecord::Schema.define do
+    create_table :uuids do |t|
+      t.string :uuid
+      t.integer :uuidable_id
+      t.string :uuidable_type, :limit => 40
+    end
+
+    create_table :cars
+
+    create_table :bikes
+  end
+end
--- test/uuid_it_test.rb
+++ test/uuid_it_test.rb
@@ -1,8 +1,37 @@
 require 'test_helper'
 
+class Car < ActiveRecord::Base
+  uuid_it
+end
+
+class Bike < ActiveRecord::Base
+end
+
 class UuidItTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test "should assing Uuids to new objects" do
+    c = Car.create
+    assert c.uuid_object
+    assert c.uuid.present?
+
+    c2 = Car.create
+    assert_not_equal c.uuid, c2.uuid
+  end
+
+  test "should assing Uuid to already existing objects on first access" do
+    b1 = nil
+    assert_difference "Uuid.count", 0 do
+      b1 = Bike.create
+    end
+    Bike.class_eval do
+      uuid_it
+    end
+    assert_nil b1.uuid_object
+    b1.uuid
+    assert b1.uuid_object
+    assert b1.uuid.present?
+
+    b2 = Bike.create
+    assert b2.uuid_object
+    assert b2.uuid.present?
   end
 end
